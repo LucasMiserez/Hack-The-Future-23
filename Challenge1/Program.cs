5a8d186dbbd69195013fd17387d7f45d60c78d26
LucasMiserez <miserez.lucas@gmail.com>
2023-11-09 10:18:28 +0100

    challenge1 1.0

--- Challenge1/Program.cs
+++ Challenge1/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 
 namespace Challenge1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static readonly HttpClient client = new();
+
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            client.BaseAddress = new Uri("https://exs-htf-2023.azurewebsites.net/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add("Authorization", "team bd486984-077b-4c27-b203-b5d1e317da74");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("/api/challenges/find-the-jeep?isTest=true");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Something went wrong");
+                return;
+            }
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
         }
     }
 }
